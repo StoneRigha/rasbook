1d86f42edc00fb2257916ff7fbfff97b0cd07b70
StoneRiga <stoneriga@gmail.com>
2022-01-22 16:06:30 +0300

    Right header update

--- components/Header.js
+++ components/Header.js
@@ -29,7 +29,7 @@ function Header (){
 
                 <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
                     <SearchIcon className="h-6 text-gray-600" />
-                    <input className="flex ml-2 bg-transparent outline-none placeholder-gray-500 flex-shrink" 
+                    <input className="hidden md:inline-flex ml-2 bg-transparent outline-none placeholder-gray-500 flex-shrink" 
                     type="text" 
                     placeholder="Search Facebook" />
                 </div>
@@ -50,9 +50,13 @@ function Header (){
 
             {/* header right (profile settings and log out icon)*/}
 
-            <div>
+            <div className="flex items-center sm:space-x-2 justify-end">
                 {/* Profile Picture */}
-                <p className="whitespace-nowrap font-semibold pr-3">Ras Stone</p>
+                <p className="whitespace-nowrap font-semibold pr-3">Rigz Maghanga</p>
+                <ViewGridIcon className="icon" />
+                <ChatIcon className="icon" />
+                <BellIcon className="icon" />
+                <ChevronDownIcon className="icon" />
             </div>
 
         </div>
