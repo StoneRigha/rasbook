465a5e3d591e3e6b4690c3afc0f6f5107a7304d8
StoneRiga <stoneriga@gmail.com>
2022-01-23 19:57:16 +0300

    Login component and update on next auth js api

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,8 @@
-import NextAuth from "next-auth";
-import Provider from "next-auth/providers";
+import FacebookProvider from "next-auth/providers/facebook";
 
-export default NextAuth({
-  // Configure one or more authentication providers
-  providers: [
-    Provider.Facebook({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-  ],
-})
+providers: [
+  FacebookProvider({
+    clientId: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+  })
+]
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import { getSession } from 'next-auth/react'
+import Login from '../components/Login'
 
 
 export default function Home({ session }) {
   if (!session) return <Login />
+
   return (
     <div >
       <Head>
@@ -27,7 +30,7 @@ export default function Home({ session }) {
 
 // Server side rendering
 export async function getServerSideProps (context){
-  // get user 
+ // get user 
   const session = await getSession (context);
 
   return {
@@ -36,3 +39,4 @@ export async function getServerSideProps (context){
     }
   }
 }
+
