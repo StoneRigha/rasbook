1159434fd3fa74d6bd0ffc0fbc33d3e4f451c2c4
StoneRiga <stoneriga@gmail.com>
2022-01-23 22:11:23 +0300

    Login component

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,12 @@
 import FacebookProvider from "next-auth/providers/facebook";
 
+
+
 providers: [
   FacebookProvider({
     clientId: process.env.FACEBOOK_CLIENT_ID,
     clientSecret: process.env.FACEBOOK_CLIENT_SECRET
   })
 ]
+
+
