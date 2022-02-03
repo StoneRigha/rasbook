82ad9ba3d6c9b838a59bb2f043d5ebf6d2cfcfef
StoneRiga <stoneriga@gmail.com>
2022-02-03 14:53:20 +0300

    Right side bar update and bug fixes

--- components/Header.js
+++ components/Header.js
@@ -14,8 +14,11 @@ import {
     TemplateIcon,
 } from "@heroicons/react/outline";
 import HeaderIcon from "./HeaderIcon";
+import { signOut, useSession} from "next-auth/react";
 
 function Header (){
+    const {data:session} = useSession();
+
     return (
         <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
 
@@ -52,6 +55,15 @@ function Header (){
 
             <div className="flex items-center sm:space-x-2 justify-end">
                 {/* Profile Picture */}
+                <Image 
+                onClick={signOut}
+                className="rounded-full cursor-pointer"
+                src={session.user.image}
+                width="40"
+                height="40"
+                layout="fixed"
+                />
+
                 <p className="whitespace-nowrap font-semibold pr-3">Rigz Maghanga</p>
                 <ViewGridIcon className="icon" />
                 <ChatIcon className="icon" />
