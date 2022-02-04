2a72b21541d1c50deaf792c5c14ee06851c4c8c7
StoneRiga <stoneriga@gmail.com>
2022-02-04 13:17:26 +0300

    input box update

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,12 +1,17 @@
 import { useSession} from "next-auth/react";
 import Image from "next/image";
+import { EmojiHappyIcon } from "@heroicons/react/outline";
+import { CameraIcon, VideoCameraIcon} from "@heroicons/react/solid";
 
 function InputBox (){
     const {data:session,} = useSession();
+    const sendPost = (e) => {
+        e.preventDefault();
+    };
 
     return (
-        <div>
-            <div>
+        <div className="bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6">
+            <div className="flex space-x-4 p-4 items-center ">
                 <Image 
                 className="rounded-full"
                 src={session.user.image}
@@ -14,7 +19,34 @@ function InputBox (){
                 height={40}
                 layout="fixed"
                 />
+                <form action="" className="flex flex-1 ">
+                    <input type="text" 
+                    className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"
+                    placeholder={`What's on your mind, ${session.user.name}?`} />
+                </form>
+                <button hidden type="submit" onClick={sendPost}>Post</button>
             </div>
+
+            <div className="flex justify-evenly p-3 border-t ">
+                <div className="inputIcon">
+                    {/* live video */}
+                    <VideoCameraIcon className="h-7 text-red-500 "/>
+                    <p className="text-xs sm:text-sm xl:text-base"> Live Video</p>
+                </div>
+
+                <div className="inputIcon">
+                    {/* photo/video */}
+                    <CameraIcon className="h-7 text-green-400" />
+                    <p className="text-xs sm:text-sm xl:text-base">Camera/Video</p>
+                </div>
+
+                <div className="inputIcon">
+                    {/* feelinf/activity */}
+                    <EmojiHappyIcon className="h-7 text-yellow-300 " />
+                    <p className="text-xs sm:text-sm xl:text-base">Feeling/Activity</p>
+                </div>
+            </div>
+
         </div>
     )
 }
